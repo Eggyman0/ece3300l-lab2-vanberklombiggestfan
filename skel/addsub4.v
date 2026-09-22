58bcdbe76e652775eca265853c9f51a2eb091378
Donna La <20007229+HeavensArie@users.noreply.github.com>
2026-09-21 19:09:28 -0700

    Changes addsub to use add module

--- skel/addsub4.v
+++ skel/addsub4.v
@@ -6,22 +6,21 @@ module addsub4 (
 	       output cout, ovf
 	       );
 
-   reg [4:0]		  C;
    reg [3:0]		add;
-   integer		      k;
    
    always @(A, B, subsel)
     begin
 
 	(subsel = 1) ? add = ~B :  add = B;
 
-	for (k = 0; k < 4; k = k+1)
-	  begin
-	     S[k] = A[k] ^ twos[k] ^ C[k];
-	     C[k+1] = (X[k] & Y[k]) | (X[k] & C[k]) | (Y[k] & C[k]);
- 	  end
-	carryout = C[4];
-	ovf = C[4] ^ C[3];
+	add4 adding(
+		.carryin(subsel),
+		.X(A),
+		.Y(add),
+		.S(X),
+		.carryout(cout),
+		.ovf(ovf)
+	)
     end
    
 endmodule
